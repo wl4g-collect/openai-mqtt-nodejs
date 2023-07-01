9f84fe54c5c22ee0ba2448d4440d1df509a31371
James Wong <jameswong1376@gmail.com>
2023-07-01 22:36:18 +0800

    feat: add support for proxy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Add this line at the beginning of your script
 require('dotenv').config();
+
 const axios = require("axios");
 const mqtt = require("mqtt");
 
@@ -9,11 +10,20 @@ let messages = []; // Store conversation history
 const maxMessageCount = 10;
 
 const http = axios.create({
-  baseURL: "https://api.openai.com/v1/chat",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${OPENAI_API_KEY}`,
-  },
+    baseURL: "https://api.openai.com/v1/chat",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
+    },
+    proxy: {
+        protocol: "http",
+        host: "127.0.0.1",
+        port: 8118,
+        auth: {
+            username: '',
+            password: '',
+        },
+    },
 });
 
 const host = "127.0.0.1";
@@ -21,12 +31,12 @@ const port = "1883";
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
 const OPTIONS = {
-  clientId,
-  clean: true,
-  connectTimeout: 4000,
-  username: "emqx",
-  password: "public",
-  reconnectPeriod: 1000,
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: "emqx",
+    password: "public",
+    reconnectPeriod: 1000,
 };
 
 const connectUrl = `mqtt://${host}:${port}`;
@@ -35,51 +45,50 @@ const client = mqtt.connect(connectUrl, OPTIONS);
 const chatGPTReqTopicPrefix = "chatgpt/request/";
 
 client.on("connect", () => {
-  console.log(`${host}, Connected`);
-  client.subscribe(chatGPTReqTopic, () => {
-    console.log(`${host}, Subscribed to topics with prefix 'chatgpt/request/'`);
-  });
+    console.log(`${host}, Connected`);
+    client.subscribe(chatGPTReqTopic, () => {
+        console.log(`${host}, Subscribed to topics with prefix 'chatgpt/request/'`);
+    });
 });
 
 client.on("message", (topic, payload) => {
-  console.log("Received Message:", topic, payload.toString());
-  // Check if the topic is not the one you're publishing to
-  if (topic.startsWith(chatGPTReqTopicPrefix)) {
-    const userId = topic.replace(chatGPTReqTopicPrefix, "");
-    messages[userId] = messages[userId] || [];
-    messages[userId].push({ role: "user", content: payload.toString() });
-    if (messages[userId].length > maxMessageCount) {
-      messages[userId].shift(); // Remove the oldest message
+    console.log("Received Message:", topic, payload.toString());
+    // Check if the topic is not the one you're publishing to
+    if (topic.startsWith(chatGPTReqTopicPrefix)) {
+        const userId = topic.replace(chatGPTReqTopicPrefix, "");
+        messages[userId] = messages[userId] || [];
+        messages[userId].push({ role: "user", content: payload.toString() });
+        if (messages[userId].length > maxMessageCount) {
+            messages[userId].shift(); // Remove the oldest message
+        }
+        genText(userId);
     }
-    genText(userId);
-  }
 });
 
 const genText = async (userId) => {
-  try {
-    const { data } = await http.post("/completions", {
-      model: "gpt-3.5-turbo",
-      messages: messages[userId],
-      temperature: 0.7,
-    });
-    if (data.choices && data.choices.length > 0) {
-      const { content } = data.choices[0].message;
-      console.log(content);
-      messages[userId].push({ role: "assistant", content: content });
-      if (messages[userId].length > maxMessageCount) {
-        messages[userId].shift(); // Remove the oldest message
-      }
-      const replyTopic = `chatgpt/response/${userId}`;
-      client.publish(replyTopic, content, { qos: 0, retain: false }, (error) => {
-        if (error) {
-          console.error(error);
+    try {
+        const { data } = await http.post("/completions", {
+            model: "gpt-3.5-turbo",
+            messages: messages[userId],
+            temperature: 0.7,
+        });
+        if (data.choices && data.choices.length > 0) {
+            const { content } = data.choices[0].message;
+            console.log("ChatGPT response:", content);
+            messages[userId].push({ role: "assistant", content: content });
+            if (messages[userId].length > maxMessageCount) {
+                messages[userId].shift(); // Remove the oldest message
+            }
+            const replyTopic = `chatgpt/response/${userId}`;
+            client.publish(replyTopic, content, { qos: 0, retain: false }, (error) => {
+                if (error) {
+                    console.error(error);
+                }
+            });
+        } else {
+            console.warn("Empty: No data received.");
         }
-      });
-    } else {
-      console.log("Empty: No data received.");
+    } catch (e) {
+        console.error("Error: No data received.", e);
     }
-  } catch (e) {
-    console.log(e);
-    console.log("Error: No data received.");
-  }
 };
